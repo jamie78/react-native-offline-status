28dd428e63bcadf807a5b88db4fbfbbf44e54db7
Jakub <jakubkraus78@gmail.com>
2018-01-19 15:47:13 +0100

    [INIT]

---
+++ index.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import {NetInfo, View, StatusBar, Animated, Easing, AppState} from 'react-native';
+import styles from './index.styles';
+import PropTypes from 'prop-types';
+import { Button, Icon, Header, Left, Body, Right } from 'native-base';
+
+export default class OfflineBar extends Component {
+  static propTypes = {
+    offlineText: PropTypes.string
+  }
+
+  animationConstants = {
+    DURATION: 800,
+    TO_VALUE: 4,
+    INPUT_RANGE: [0, .5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
+    OUTPUT_RANGE: [0, -15, 0, 15, 0, -15, 0, 15, 0]
+  }
+
+  setNetworkStatus = (status) => {
+    this.setState({isConnected: status});
+    if (status) {
+      this.triggerAnimation();
+    }
+  }
+
+  state = {
+    isConnected: true
+  }
+  _handleAppStateChange = (nextAppState) => {
+    if (nextAppState === 'active') {
+      NetInfo.isConnected.fetch().then(this.setNetworkStatus);
+    }
+  }
+  componentWillMount () {
+    NetInfo.isConnected.addEventListener('change', this.setNetworkStatus);
+    AppState.addEventListener('change', this._handleAppStateChange);
+    this.animation = new Animated.Value(0);
+  }
+  componentWillUnMount () {
+    NetInfo.isConnected.removeEventListener('change', this.setNetworkStatus);
+    AppState.removeEventListener('change', this._handleAppStateChange);
+  }
+  // Took Reference from https://egghead.io/lessons/react-create-a-button-shake-animation-in-react-native#/tab-code
+  triggerAnimation = () => {
+    this.animation.setValue(0);
+    Animated.timing(this.animation, {
+      duration: this.animationConstants.DURATION,
+      toValue: this.animationConstants.TO_VALUE,
+      useNativeDriver: true,
+      ease: Easing.bounce
+    }).start();
+  }
+
+  render () {
+    const interpolated = this.animation.interpolate({
+      inputRange: this.animationConstants.INPUT_RANGE,
+      outputRange: this.animationConstants.OUTPUT_RANGE
+    });
+    const animationStyle = {
+      transform: [{translateX: interpolated}]
+    };
+    const {offlineText = 'You are offline.'} = this.props;
+    return !this.state.isConnected ?
+        <Header style={styles.container}>
+          <Left />
+          <Body>
+            <Animated.Text style={[styles.offlineText, animationStyle]}>
+              {offlineText}
+            </Animated.Text>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon style={styles.icon} name="refresh" />
+            </Button>
+          </Right>
+        </Header> : null;
+  }
+}
---
+++ index.styles.js
@@ -0,0 +1,18 @@
+export default {
+  container: {
+    backgroundColor: '#424242',
+    borderBottomWidth: 0,
+    height: 55,
+  },
+  offlineText: {
+    color: 'white',
+    fontSize: 15,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    color: 'white',
+    alignSelf: 'center',
+    justifyContent: 'flex-end',
+  },
+};
